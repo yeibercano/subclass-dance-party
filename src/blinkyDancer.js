137a59f384015ccbc8ea9f914108c11d273bd919
yeibercano <yeibercano@gmail.com>
2016-03-22 10:57:40 -0700

    progress! Dancers now refactored!!!

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -5,8 +5,7 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-
-  console.log('BlinkyDancer this: ', this)
+  // console.log('BlinkyDancer this: ', this)
 
 };
 
--- src/dancer.js
+++ src/dancer.js
@@ -1,20 +1,20 @@
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps){
-  // Dancer.prototype.constructor = Dancer;
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   console.log('Dancer this: ', this)
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.top = top;
+  this.left = left;
+  this.step();
+  this.setPosition(top, left);
 };
-
-
 Dancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   setTimeout(this.step, this.timeBetweenSteps);
 };
-
 Dancer.prototype.setPosition = function(top, left){
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
